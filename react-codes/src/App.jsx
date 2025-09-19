5da5648929e0e186f2e4e4d76eaa8eb56d15d978
Shubham Swaroop Bhatnagar <69385050+ssbhatnagar@users.noreply.github.com>
2025-09-19 16:15:47 +0000

    added globe view and new questions

--- react-codes/src/App.jsx
+++ react-codes/src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css'
+// src/App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Components
+import GlobeView from "./components/GlobeView";
+import Calculator from "./questions/Calculator";
+import ToDo from "./questions/ToDo";
 
 function App() {
-  return(
-    <div>
-      <p>Hi</p>
-    </div>
-  )
+  return (
+    <Routes>
+      {/* Home Globe Page */}
+      <Route path="/" element={<GlobeView />} />
+
+      {/* Projects */}
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/todo" element={<ToDo />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ react-codes/src/components/GlobeView.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Globe from "react-globe.gl";
+import { useNavigate } from "react-router-dom";
+
+const projects = [
+  { name: "Todo App", lat: 28.6, lng: 77.2, route: "/todo" }, // Delhi
+  { name: "Calculator", lat: 19.0, lng: 72.8, route: "/calculator" }, // Mumbai
+];
+
+const GlobeView = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        labelsData={projects}
+        labelLat={(d) => d.lat}
+        labelLng={(d) => d.lng}
+        labelText={(d) => d.name}
+        labelSize={1.5}
+        onLabelClick={(d) => navigate(d.route)}
+      />
+    </div>
+  );
+};
+
+export default GlobeView;
