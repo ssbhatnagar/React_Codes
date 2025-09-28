632caf4af5f041620f6390385d14931db38cf4d0
Shubham Swaroop Bhatnagar <69385050+ssbhatnagar@users.noreply.github.com>
2025-09-28 07:20:29 +0000

    feat: skeleton checkout and cart

---
+++ Cart_Checkout_Context/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+import Body from './components/Body'
+
+function App() {
+ 
+  
+
+  return (
+    <>
+      <Body />
+
+    </>
+  )
+}
+
+export default App
