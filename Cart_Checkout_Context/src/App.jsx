b4d2966f6a1afeed0752c7cb8a7b03a9207b717e
Shubham Swaroop Bhatnagar <69385050+ssbhatnagar@users.noreply.github.com>
2025-10-09 20:03:03 +0000

    feat: added cart proj

--- Cart_Checkout_Context/src/App.jsx
+++ Cart_Checkout_Context/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState, useEffect } from 'react'
-
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
 import Body from './components/Body'
+// import {CartProvider } from './ contexts/CartContext';
 
 function App() {
  
-  
-
   return (
-    <>
-      <Body />
-
-    </>
+    <BrowserRouter> 
+      {/* <CartProvider> */}
+        <Body />
+      {/* </CartProvider> */}
+    </BrowserRouter>
   )
 }
 
