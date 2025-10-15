6850211475ae6f462532856cc8cf7a48246f1e7d
Shubham Swaroop Bhatnagar <69385050+ssbhatnagar@users.noreply.github.com>
2025-10-15 20:52:02 +0000

    feat: added star raiting

--- CartContext/src/Components/Header.jsx
+++ CartContext/src/Components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import styles from '../CSS/Header.module.css'
+import Cart from './Cart.jsx'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className={styles.centerContainer}>
+    <div className={styles.mainHeaderDiv}>
+      <h1 className={styles.heading}>THE WEIRED ONLINE SHOP</h1>
+    </div>
+    <Cart/>
+    </div>
   )
 }
 
---
+++ CartContext/src/Contexts/CartContext.jsx
@@ -0,0 +1,8 @@
+import {useState, createContext, useContext, useEffect} from 'react'
+
+export function CartContext() {
+  return (
+    <div>CartContext</div>
+  )
+}
+
